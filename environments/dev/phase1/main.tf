5c437ead8c011f8cddb013888b4c40c2ff3a6ba0
venkyredd <146801831+venkyredd@users.noreply.github.com>
2025-05-08 22:09:46 +0530

    Update main.tf

--- environments/dev/phase1/main.tf
+++ environments/dev/phase1/main.tf
@@ -1,34 +1,34 @@
 module "vpc" {
-  source = "../../modules/vpc"
+  source      = "../../../modules/vpc"
   name_prefix = "v-crud"
   # other vars
 }
 
 module "security_groups" {
-  source     = "../../modules/security_groups"
-  vpc_id     = module.vpc.vpc_id
+  source      = "../../../modules/security_groups"
+  vpc_id      = module.vpc.vpc_id
   name_prefix = "v-crud"
 }
 
 module "ecs_cluster" {
-  source = "../../modules/ecs_cluster"
+  source      = "../../../modules/ecs_cluster"
   name_prefix = "v-crud"
 }
 
 module "ecr" {
-  source = "../../modules/ecr"
+  source      = "../../../modules/ecr"
   name_prefix = "v-crud"
 }
 
 module "alb" {
-  source = "../../modules/alb"
-  vpc_id = module.vpc.vpc_id
-  public_subnets = module.vpc.public_subnets
+  source                   = "../../../modules/alb"
+  vpc_id                   = module.vpc.vpc_id
+  public_subnets           = module.vpc.public_subnets
   shared_security_group_id = module.security_groups.shared_security_group_id
-  name_prefix = "v-crud"
+  name_prefix              = "v-crud"
 }
 
 module "iam" {
-  source = "../../modules/iam"
+  source      = "../../../modules/iam"
   name_prefix = "v-crud"
 }
