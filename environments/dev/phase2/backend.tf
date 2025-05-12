7269cc947644eff14ecff1f8cef80aafb171ec91
venkyredd <146801831+venkyredd@users.noreply.github.com>
2025-05-12 23:01:58 +0530

    Update backend.tf

--- environments/dev/phase2/backend.tf
+++ environments/dev/phase2/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = v-backend-s3
+    bucket         = v-infra-backend
     key            = "infra/terraform.tfstate"
     region         = var.aws_region
     encrypt        = true
